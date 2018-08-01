8ce5308099d376bd39f0907d08067be93ac67edf
Robert Smol <robert.smol@stereoit.com>
2018-08-01 23:35:45 +0200

    Added SCSS pipeline

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("static/css"))
+        .pipe(browserSync.stream());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function () {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+        .pipe(gulp.dest("static/js"))
+        .pipe(browserSync.stream());
+});
